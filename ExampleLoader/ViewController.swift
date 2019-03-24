e3df86776c7d9283cc04ec10b2120ec7a18eb9fb
AbdulBasitTrialx <basit@trialx.com>
2019-03-24 23:46:16 +0530

    Initial Commit

---
+++ ExampleLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleLoader
+//
+//  Created by Abdul Basit on 24/03/19.
+//  Copyright © 2019 Abdul Basit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
